1654000a985cce3e95fa2a76b63978b75a97f4a6
deebajee2009 <deebajee2009@gmail.com>
2025-01-28 21:24:54 +0330

    Writing API fetch class in utils

--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,16 @@
+import requests
+
 
 def get_current_gold_price(self):
         # Example function to get the current gold price
         return 10000000  # This could be fetched from an API or database
+
+class PriceFetchService:
+    # Api for gold, currencies, cryptocurrencies proces in Toman from http://brsapi.ir
+    PRICE_API_URL = 'https://brsapi.ir/FreeTsetmcBourseApi/Api_Free_Gold_Currency.json'
+
+    @staticmethod
+    def fetch_price():
+        response = requests.get(PriceService.GOLD_CURRENCY_API_URL)
+        data = response.json()
+        return data
